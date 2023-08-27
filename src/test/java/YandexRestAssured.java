7fbeb7b5ee179f64645d4060d6803209a733a687
Alexey Kravchenko <alexfamous88@gmail.com>
2023-08-27 12:25:53 +0300

    add RA

--- src/test/java/YandexRestAssured.java
+++ src/test/java/YandexRestAssured.java
@@ -1,8 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+
 public class YandexRestAssured {
 
     public static final String LOGIN = "popk3n";
     public static final String PASSWORD = "wAMVqoC";
 
+    @Test
+    public void testFilesInDisk() {
 
+        // Авторизация
+
+
+        // Получение списка всех файлов на Диске
+
+        Response response = RestAssured.get("https://cloud-api.yandex.net/v1/disk/resources/files").andReturn();
+        response.prettyPrint();
+
+        // Разлогин
+
+    }
 
 }
